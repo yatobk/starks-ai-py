c76c4d4fb0757bdcc1cf46ba2a879b95f94f53f5
yatobk <natarenner.nr@gmail.com>
2024-02-17 11:08:01 -0300

    new dir format

--- src/api/index.py
+++ src/api/index.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, ValidationError
 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..' )))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..' )))
 
 from lib.langchain.agents.OpenAiAgent import answer
 
--- src/app/index.py
+++
@@ -1,8 +0,0 @@
-from api.routes import app
-#import sys
-#import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-#from lib.utils.envSchema import EnvConfig
-if __name__ == "__main__":
-    #EnvConfig()
-    app.run(debug=True)
